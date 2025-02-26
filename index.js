9e51b65652650f98b5edca639d641688562c10e0
Mikea <109756114+mikeaca@users.noreply.github.com>
2025-02-26 17:35:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
+let scorA = document.getElementById("scoreA")
+let scorB = document.getElementById("scoreB")
 let scrA = 0
 let scrB = 0
 
 function addA1() {
     scrA += 1
-    document.getElementById("scoreA").textContent = scrA
+    scorA.textContent = scrA
     
 }
 
 function addA2() {
     scrA += 2
-    document.getElementById("scoreA").textContent = scrA
+    scorA.textContent = scrA
 }
 
 function addA3() {
     scrA += 3
-    document.getElementById("scoreA").textContent = scrA
+    scorA.textContent = scrA
 }
 
 function addB1() {
     scrB += 1
-    document.getElementById("scoreB").textContent = scrB
+    scorB.textContent = scrB
     
 }
 
 function addB2() {
     scrB += 2
-    document.getElementById("scoreB").textContent = scrB
+    scorB.textContent = scrB
 }
 
 function addB3() {
     scrB += 3
-    document.getElementById("scoreB").textContent = scrB
+    scorB.textContent = scrB
 }
